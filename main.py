96e036171e13b52b37c45e6214637041c60cbc4f
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-09-20 18:49:09 +0530

    first step talking to my assistant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pyttsx3 as p
-
+import speech_recognition as sr
 engine = p.init()
 
 rate = engine.getProperty('rate')
@@ -7,7 +7,27 @@ engine.setProperty('rate', 180)
 voices = engine.getProperty('voices')
 voiceList = engine.setProperty('voice',voices[1].id)
 
-print(voiceList)
+# print(voiceList)
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
-engine.say("Hello there i am your voice assistant")
-engine.runAndWait()
+
+# create a instance of the recognizer class
+r = sr.Recognizer()
+
+
+speak("Hello sir, I'm your voice assistant The Professor, how are you")
+
+#main code starts here
+with sr.Microphone() as source:
+    r.energy_threshold = 10000
+    r.adjust_for_ambient_noise(source, 1.2)
+    print("Lisenting...")
+    audio = r.listen(source)
+    text = r.recognize_google(audio)
+    print(text)
+    
+if 'what' and 'about' and 'you' in text:
+    speak('I am also having a good day sir')
+speak('what can i do for you?')
