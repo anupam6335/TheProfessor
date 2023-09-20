2fce78f08be0e00ed0ba6e0215b9222d42d9574f
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-09-20 22:20:44 +0530

    Getting Local Path

--- theprofessor.py
+++ theprofessor.py
@@ -2,11 +2,18 @@ from tkinter import *
 from tkinter import filedialog
 
 
+def get_path():
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
+
 root = Tk()
 root.title('The Professor - The Video Downloader 😎')
 canvas = Canvas(root,width=500,height=400)
 canvas.pack()
 canvas.configure(bg='#5C8374')
+
+
+
 #app label
 app_label = Label(root, text='The Professor 😎',fg='#26577C',font=('Cascadia Code 25 bold', 20)).place(x=180, y=20)
 canvas.create_window(200, 20, window=app_label)
@@ -22,8 +29,15 @@ canvas.create_window(200, 100, window=url_entry)
 
 
 #path to download videos
-path_label = Label(root, text="Select path to download",font='Helvetica 11 bold').place(x = 180, y = 130) 
-path_button = Button(root, text='Select', width=10).place(x = 220, y = 160) 
+# Create the path label
+path_label = Label(root, text="Select path to download", font='Helvetica 11 bold')
+
+# Place the path label
+path_label.place(x=180, y=130)
+
+# Create the path button
+path_button = Button(root, text='SELECT', width=10, command=get_path).place(x = 220, y = 160) 
+
 canvas.create_window(200, 150, window=path_label)
 canvas.create_window(200, 170, window=path_button)
 
